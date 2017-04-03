4f244771ba38e9924caf77666b80d02493a8e80e
Mus M <mus-m@outlook.com>
2017-04-02 22:51:21 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -203,6 +203,7 @@ let
 
     write_chunked(stream, "ab")
     write_chunked(stream, "cde")
+    write_chunked(stream, "")
 
     response = JSON.parse(readstring(stream))
     @test response["data"] == "abcde"
